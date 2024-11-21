478d43c1faf723105e0da5fd2b5719ced4dd430e
anim200 <126519717+anim200@users.noreply.github.com>
2024-11-21 22:44:54 +0600

    Update index.js

--- booking-api/index.js
+++ booking-api/index.js
@@ -13,8 +13,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-    origin: "https://tour-booking-frontend-murex.vercel.app", // Use the domain only
-    credentials: true, // Allow credentials like cookies or headers
+  origin: 'https://tour-booking-frontend-murex.vercel.app', // Allow only your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true, // If you're using cookies or sessions
 };
 
 
