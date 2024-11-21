b1226491d9944c7ddfbf09469f43fd4a4a550314
anim200 <126519717+anim200@users.noreply.github.com>
2024-11-22 02:31:41 +0600

    Update index.js

--- booking-api/index.js
+++ booking-api/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-  origin: 'https://tour-booking-frontend-umber.vercel.app/',
+  origin: 'https://tour-booking-frontend-umber.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // Allow cookies or credentials
 };
