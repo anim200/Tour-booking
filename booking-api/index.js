ab0353db26a8f9ca6a499be6a3cbcc790e5b4ab8
anim200 <126519717+anim200@users.noreply.github.com>
2024-11-21 23:42:41 +0600

    Update index.js

--- booking-api/index.js
+++ booking-api/index.js
@@ -3,51 +3,50 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import tourRoute from './routes/tours.js';
+import userRoute from './routes/users.js';
+import authRoute from './routes/auth.js';
+import reviewRoute from './routes/reviews.js';
+import bookingRoute from './routes/bookings.js';
 
 dotenv.config();
 const app = express();
-
-// CORS Configuration
+const port = process.env.PORT || 3000;
 const corsOptions = {
   origin: 'https://tour-booking-frontend-murex.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // Allow cookies or credentials
 };
-
-app.use(cors(corsOptions));
-
-// Cookie Parser Middleware
-app.use(express.json());
-app.use(cookieParser());
-
-// Routes
-app.use('/api/v1/auth', require('./routes/auth'));
-app.use('/api/v1/tours', require('./routes/tours'));
-app.use('/api/v1/users', require('./routes/users'));
-app.use('/api/v1/review', require('./routes/reviews'));
-app.use('/api/v1/booking', require('./routes/bookings'));
-
-// Root route
-app.get('/', (req, res) => {
-    res.send('App is working very fine');
-});
-
-const port = process.env.PORT || 3000;
-
-// Start server
-app.listen(port, () => {
-    console.log('Server listening on port', port);
-});
-
 // Database connection
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
-        console.log('MongoDB database connected');
+        console.log('MongoDB database connected to booking');
     } catch (err) {
         console.log(err);
     }
 };
 
-// Connect to database after server starts
-app.on('listening', connect);
+// Middleware
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// Routes
+app.use('/api/v1/auth',authRoute)
+app.use('/api/v1/tours', tourRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/review', reviewRoute);
+app.use('/api/v1/booking', bookingRoute);
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('App is working');
+});
+
+// Start server
+app.listen(port, () => {
+    connect(); // Connect to the database
+    console.log('Server listening on port', port);
+});
+
